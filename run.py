40b504bf7f08eb6d4c60e51ec99443853c55f22e
Niall Carroll <niallpcarroll@gmail.com>
2024-07-08 18:22:34 +0000

    Add inital game input to run.py

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+name = input(f"Enter your name here: ")
+print("Welcome,", name)
+game = input(f"Choose which maths game you would like to play: Addition (+), Subtraction (-), Multiplication (x), Division (/):")
+if game == "+":
+    print("Let's play an addition game!")
+elif game == "-":
+    print("Let's play a subtraction game!")
+elif game == "x":
+    print("Let's play a multiplication game!")
+elif game == "/":
+    print("Let's play a division game!")
+else: print("Please choose a valid game type (+,-,x,/)")            
